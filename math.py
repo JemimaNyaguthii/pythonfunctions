1255d7b7f67c6f5020ad877963819379c2f4bb20
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-03-29 19:38:20 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    answer=a+b
+    return answer
+def subtract(c,d):
+    answer=c-d
+    return answer
+
+def divide(a,b):
+    answer= a/b
+    return answer 
+
+def multiply(c,d):
+    answer=c*d
+    return answer
+
+def remainder(c,d):
+    answer=c%d
+    return answer
+
+
